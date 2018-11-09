67a48809cd119ac862076c1e27c0c610fac08966
santoshsunagartarent <santosh.sunagar@tarento.com>
2018-11-09 13:26:42 +0530

    change service mogodb service

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-var mongodbservice = require('./mongodbservice');
+var mongodbservice = require('./mongodbService');
 var hitlService = require('./hitlService');
 const index = require('./index')
 
