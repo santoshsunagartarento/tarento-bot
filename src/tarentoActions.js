253906ff1bc60ab55543e166d7770090d25ebcc1
santoshsunagartarent <santosh.sunagar@tarento.com>
2018-11-13 11:54:28 +0530

    Added new flow for tarento bot

---
+++ src/tarentoActions.js
@@ -0,0 +1,10 @@
+const _ = require('lodash')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var mongodbservice = require('./mongodbService');
+var hitlService = require('./hitlService');
+const index = require('./index')
+
+module.exports = {
+
+}
