58934926041a25e94c1f87269a9bfb096b6277cc
santoshsunagartarent <santosh.sunagar@tarento.com>
2018-11-13 16:00:31 +0530

    Added changes for delayed reply and start message

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { contentRenderers, setup } = require('@botpress/builtins')
 const renderers = require('./renderers')
 const actions = require('./actions')
 const tarentoActions = require('./tarentoActions')
+var timeOut = 0;
 
 module.exports = bp => {
   ////////////////////////////
@@ -90,7 +91,13 @@ module.exports = bp => {
     bp.hear(
     { type: /bp_dialog_timeout|text|message|quick_reply|attachment|postback|referral|feed/i },
     async (event, next) => {
-      event.reply('#!client-queries-4dm7gd')
+      timeOut++;
+      if(timeOut > 1) {
+        timeOut = 0;
+        event.reply('#!client-queries-4dm7gd');
+      } else {
+        event.reply('#!client-queries-4dm7gd');
+      }
     })
 
 
