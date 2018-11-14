6ac850e64ee9fe030732308e20a723655f416e51
santoshsunagartarent <santosh.sunagar@tarento.com>
2018-11-14 12:19:59 +0530

    Added changed files

--- src/index.js
+++ src/index.js
@@ -22,12 +22,12 @@ module.exports = bp => {
   jsdoc.explain({ files: [__dirname + '/actions.js'] }).then(docs => {
     bp.dialogEngine.registerActionMetadataProvider(fnName => {
       const meta = docs.find(({ name }) => name === fnName)
-      return {
+      /* return {
         desciption: meta.description,
         params: (meta.params || [])
           .filter(({ name }) => name.startsWith('args.'))
           .map(arg => ({ ...arg, name: arg.name.replace('args.', '') }))
-      }
+      } */
     })
     bp.dialogEngine.registerFunctions(actions)
     bp.dialogEngine.registerFunctions(tarentoActions)
@@ -80,11 +80,24 @@ module.exports = bp => {
     })
     //End: Added changes for Human in the loop
 
-    
-  // bp.hear(/.+/i, async (event, next) => {
-  //   bp.messenger.sendText(event.user.id, "Hi, "+event.text + )
-  // })
-    // By not calling next() here, we "swallow" the event (won't be processed by the dialog engine below)
-  
+    //Begin: Added for start message
+    bp.hear({ type: /visit/i }, async (event, next) => {
+      event.reply('#!text-2aQzoP')
+      next()
+    })
+    //End: Added for start message
+     //Begin: Added for dialog timeout
+    bp.hear(
+    { type: /bp_dialog_timeout|text|message|quick_reply|attachment|postback|referral|feed/i },
+    async (event, next) => {
+      timeOut++;
+      if(timeOut > 1) {
+        timeOut = 0;
+        event.reply('#!client-queries-wpQcMd');
+      } else {
+        event.reply('#!client-queries-nlwN9M');
+      }
+    })
+    //End: Added for dialog timeout
 
 }
