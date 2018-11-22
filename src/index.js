4b817981aaeda3dcc7c5d36f3696abddc69fe9d5
santoshsunagartarent <santosh.sunagar@tarento.com>
2018-11-22 15:07:32 +0530

    Added custom analytics for app

--- src/index.js
+++ src/index.js
@@ -99,4 +99,22 @@ let timeOut =0;
     })
     //End: Added for dialog timeout
 
+    //Begin: Added for analytics
+    bp.analytics.custom.addGraph({
+    name: 'Number of conversations',
+    type: 'count',
+    description: 'Number of conversations with each user',
+    variables: ['conversation']
+  })
+
+  bp.analytics.custom.addGraph({
+    name: 'Misunderstood Messages',
+    type: 'percent',
+    description: 'Percentage of misunderstood messages',
+    variables: ['misunderstood', 'conversation'],
+    fnAvg: (misunderstood, conversations) => misunderstood / conversations
+  })
+  //End: Added for analytics
+
+
 }
